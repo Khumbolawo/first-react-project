61cd93eb2f7229604cec126c6550ab82ffac5be3
Khumbolawo <khumbo87@gmail.com>
2024-03-03 22:28:41 +0200

    First React program to track my learning

---
+++ react-app/src/App.tsx
@@ -0,0 +1,10 @@
+import ListGroup from "./components/ListGroup";
+
+//code to import message function into app function, then export app for
+//use elsewhere
+function App(){
+  return <div><ListGroup/></div>
+}
+
+//thise code prints hello world on our react webpage
+export default App;
---
+++ react-app/src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCasing - react convention for naming function components
+
+function Message(){
+    //example of JSX component - javascript XML
+    //code gets compiled to javascript
+    return <h1>Hello World</h1>;
+}
+
+//code to send function to app
+export default Message;
